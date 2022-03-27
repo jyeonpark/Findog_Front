4055a1d252897ae6d2300dc4a3bfdddcb9163681
jyeonpark <cute060500@naver.com>
2022-03-27 19:39:25 +0900

    회원가입 ui 완성

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import Header from "./components/Header";
+import { Login } from "./pages/Login";
+import Tab from "./components/Tab";
 import SignUp from './pages/SignUp';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header />
+    <Tab />
     <SignUp />
   </React.StrictMode>,
   document.getElementById("root")
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,33 @@
-import React, { Component } from "react";
-import Header from "../components/Header";
-import Tab from "../components/Tab";
+import React, {Component} from "react";
+import styles from "../styles/Login.module.css";
 
-function Login() {
-  return (
-    <div className="Login">로그인</div>
-  );
+
+export const Login = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.header}>
+                로그인
+            </div>
+            <div className={styles.boxContainer}>
+                <div>
+                    <input className={styles.box} placeholder="아이디"/>
+                </div>
+                <div>
+                    <input className={styles.box} placeholder="비밀번호"/>
+                </div>
+            </div>
+            <button className={styles.loginBtn}>로그인</button>
+            <div className={styles.one}>
+                <span className={styles.two}>회원가입</span>
+                <div>
+                    <span className={styles.three}>아이디 찾기</span>
+                    <span>|</span>
+                    <span className={styles.four}>비밀번호 찾기</span>
+                </div>
+            </div>
+        </div>
+    );
+
 }
 
 export default Login;
