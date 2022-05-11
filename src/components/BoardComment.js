9d8d99c5ce3ab99ce75463b1c2178194f7829834
jyeonpark <cute060500@naver.com>
2022-05-11 14:40:25 +0900

    feat: 댓글 조회, 댓글 작성하기(대댓글 없을때)
    부모 댓글 밑에 대댓글이 보이도록 조회 api 완성
    대댓글 제외한 부모 댓글 작성하기 완성

--- src/components/BoardComment.js
+++ src/components/BoardComment.js
@@ -1,11 +1,57 @@
-import  React, {Fragment } from "react";
+import React, {
+  Fragment,
+  useState,
+  useEffect,
+  useCallback,
+  useRef,
+  ReactText,
+} from "react";
+import API from "./../utils/api";
+import styled from "styled-components";
+import { CommentWrite } from "./CommentWrite";
+import { SingleComment } from "./SingleComment";
+import { ReplyComments } from "./ReplyComments";
 
-export const BoardComment = () => {
-    return (
-        <Fragment>
-            <div>
-                hi
-            </div>
-        </Fragment>
-    )
-}
+
+const Container = styled.div`
+  width: 1000px;
+  height: auto;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 80px;
+`;
+
+export const BoardComment = ({ postId }) => {
+  const [comments, setComments] = useState([]);
+
+  // 서버에서 댓글 조회하기
+  const getComments = () => {
+    API.get("/comments", { params: { postId: postId } }).then((response) => {
+      if (response.data.isSuccess) {
+        console.log(response.data);
+        setComments(response.data.result);
+      } else {
+        console.log(response);
+      }
+    });
+  };
+  useEffect(getComments, []);
+
+  return (
+    <Container>
+      <div>
+        {comments.map((item) => {
+          if (item.parentCommentId === 0) {
+            return (
+              <div >
+                <SingleComment comment={item} postId={postId}></SingleComment>
+                <ReplyComments commentList={comments} parentCommentId={item.commentId} postId={postId}></ReplyComments>
+              </div>
+            );
+          }
+        })}
+        <CommentWrite parentCommentId={0} postId={postId}></CommentWrite>
+      </div>
+    </Container>
+  );
+};
