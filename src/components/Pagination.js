c67ace4b2ab9e070e3ca399bc82c514f020aeda6
jyeonpark <cute060500@naver.com>
2022-05-15 01:46:33 +0900

    feat: pagination 구현
    총게시물개수, 한 페이지에 받아오는 게시물 수로 pagination 구현
    한 번에 5개 페이지를 보여줌
    왼쪽, 오른쪽 화살표 선택시 5칸씩 건너뛰기
    첫 번째, 마지막 페이지가 포함되었을 때는 화살표를 disabled 하게 구현

---
+++ src/components/Pagination.js
@@ -0,0 +1,129 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+
+function Pagination({ total, page, setPage }) {
+  const PAGES_PER_LIST = 5;
+  const [showingNum, setShowingNum] = useState({
+    start: 1,
+    end: PAGES_PER_LIST,
+  });
+
+  const arrowHandler = (prev, sign, totalPage) => {
+    const nextIndex = prev.end + PAGES_PER_LIST;
+    let res;
+    if (sign === 1) {
+      res = nextIndex > totalPage ? totalPage : nextIndex;
+    } else if (sign === -1) {
+      res =
+        prev.end - prev.start < 4
+          ? prev.start + 4 - PAGES_PER_LIST
+          : prev.end - PAGES_PER_LIST;
+    }
+    return { ...prev, start: prev.start + PAGES_PER_LIST * sign, end: res };
+  };
+
+  const changePageNumbersBackward = () => {
+    page > PAGES_PER_LIST &&
+      setShowingNum((prev) => arrowHandler(prev, -1, total));
+  };
+
+  const changePageNumberForward = () => {
+    showingNum.end <= total &&
+      setShowingNum((prev) => arrowHandler(prev, 1, total));
+  };
+
+  useEffect(() => {
+    const lessThanFive = total <= PAGES_PER_LIST;
+    lessThanFive
+      ? setShowingNum((prev) => ({ ...prev, start: 1, end: total }))
+      : setShowingNum((prev) => ({ ...prev, start: 1, end: PAGES_PER_LIST }));
+  }, [total]);
+
+
+  useEffect(() => {
+    setPage(showingNum.start);
+  }, [showingNum, setPage]);
+
+  const isFirstPage = showingNum.start === 1;
+  const isLastPage = showingNum.end === total;
+  const pages = getEmptyArray(showingNum.start, showingNum.end);
+  
+  function getEmptyArray(start, end){
+      const array = [];
+      for (let i=start; i<=end; i++){
+        array.push(i);
+      }
+      return array;
+  }
+
+  function PageButton({ page, setCurrentPage, isActive }) {
+    const handleClickButton = () => {
+      setCurrentPage(page);
+    };
+  
+    return (
+      <Nav isActive={isActive}>
+        <Button onClick={handleClickButton} isActive={isActive}>
+          {page}
+        </Button>
+      </Nav>
+    );
+  }
+
+
+  return (
+    <>
+      <Nav>
+        <Button onClick={changePageNumbersBackward} disabled={isFirstPage}>
+          &lt;
+        </Button>
+        {pages.map((p, idx) => {
+        return (
+          <PageButton
+            key={`pageNumber-${idx + 1}`}
+            page={p}
+            setCurrentPage={setPage}
+            isActive={p === page}
+          />
+        );
+      })}
+        <Button onClick={changePageNumberForward} disabled={isLastPage}>
+          &gt;
+        </Button>
+      </Nav>
+    </>
+  );
+}
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+  margin: 16px;
+`;
+
+const Button = styled.button`
+  border: none;
+  border-radius: 8px;
+  padding: 12px;
+  margin: 0;
+  background: ${(props) => (props.isActive ? "orange" : "rgba(211,211,211,0.5)")};
+  color: white;
+  font-size: 1rem;
+  color:  ${(props) => (props.isActive ? "white" : "black")};
+
+  &:hover {
+    background: orange;
+    cursor: pointer;
+    transform: translateY(-2px);
+  }
+
+  &[disabled] {
+    background: rgba(211,211,211,0.5);
+    cursor: revert;
+    transform: revert;
+  }
+`;
+
+export default Pagination;
--- src/pages/Board.js
+++ src/pages/Board.js
@@ -1,10 +1,11 @@
-import React, { Component, useState } from "react";
-import OptionTab from "../components/OptionTab";
+import React, { Component, useState, useEffect } from "react";
 import { Fragment } from "react";
+import OptionTab from "../components/OptionTab";
 import { Link } from "react-router-dom";
 import { BoardBox } from "../components/BoardBox";
 import styled from "styled-components";
-import { propTypes } from "react-bootstrap/esm/Image";
+import API from "../utils/api";
+import Pagination from "./../components/Pagination";
 
 const BoardContainer = styled.div`
   padding-bottom: 100px;
@@ -16,36 +17,52 @@ const BoardBody = styled.div`
 
 export const Board = () => {
   const postId = 92;
+  var size = 5;
+  const [postCount, setPostCount] = useState(0);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    API.get("/boards/count").then((response) => {
+      if (response.data.isSuccess) {
+        console.log(response.data.result);
+        setPostCount(response.data.result);
+      } else {
+        alert("인터넷 연결에 실패했습니다.");
+      }
+    });
+  }, []);
+
+  console.log("현재 페이지",page);
 
   return (
-    <BoardContainer>
-      <Fragment>
-        <OptionTab
-          FilterVisibility
-          WriteVisibility
-          InterestText="관심 목록 보기"
-        ></OptionTab>
-      </Fragment>
-      <BoardBody>
-        <Link to={"/board/detail/" + postId} style={{ textDecoration: 'none', color:"black"}}>
-          <BoardBox />
-        </Link>
-        <BoardBox />
-        <BoardBox />
-        <BoardBox />
-        <BoardBox />
-      </BoardBody>
-    </BoardContainer>
+    <div>
+      <BoardContainer>
+        <Fragment>
+          <OptionTab
+            FilterVisibility
+            WriteVisibility
+            InterestText="관심 목록 보기"
+          ></OptionTab>
+        </Fragment>
+        <BoardBody>
+          <Link
+            to={"/board/detail/" + postId}
+            style={{ textDecoration: "none", color: "black" }}
+          >
+            <BoardBox />
+          </Link>
+        </BoardBody>
+      </BoardContainer>
+
+      <footer>
+        <Pagination
+          total={postCount}
+          page={page}
+          setPage={setPage}
+        />
+      </footer>
+    </div>
   );
 };
 
 export default Board;
-
-
-BoardBox.defaultProps = {
-  postId: '0'
-}
-
-BoardBox.propsTypes = {
-  postId: propTypes.number
-}
