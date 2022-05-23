0719d6413a534839e519c80b0617b9a5d99e1d65
jyeonpark <cute060500@naver.com>
2022-05-23 19:41:29 +0900

    fix: gitignore 파일 수정

---
+++ build/precache-manifest.563f9a04f492ca5ad057bdfcb3b43196.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "00c0e1b0a3a695ca2188",
+    "url": "./static/css/main.e59b4ff1.chunk.css"
+  },
+  {
+    "revision": "00c0e1b0a3a695ca2188",
+    "url": "./static/js/main.7d28c0fc.chunk.js"
+  },
+  {
+    "revision": "667b627d902b8551ec4f",
+    "url": "./static/js/runtime~main.d434a3ac.js"
+  },
+  {
+    "revision": "4350ac932c1936c71524",
+    "url": "./static/js/2.21e706d4.chunk.js"
+  },
+  {
+    "revision": "5dac4e7d22802ab88f5a",
+    "url": "./static/js/3.7eb544a6.chunk.js"
+  },
+  {
+    "revision": "5405d77c51fb46a0cbf26cb96fe4da4d",
+    "url": "./static/media/profileImage.5405d77c.png"
+  },
+  {
+    "revision": "69099a8a201b1effdc4e3eb904f63bb5",
+    "url": "./static/media/dog2.69099a8a.jpeg"
+  },
+  {
+    "revision": "85950ef040330ec9335a380caadff169",
+    "url": "./static/media/header_dog.85950ef0.jpg"
+  },
+  {
+    "revision": "e6f73a997513923fff0f725b9cef6c5c",
+    "url": "./static/media/doghouse.e6f73a99.png"
+  },
+  {
+    "revision": "aadd915b7584ca3b21897c7115c2501a",
+    "url": "./index.html"
+  }
+];
